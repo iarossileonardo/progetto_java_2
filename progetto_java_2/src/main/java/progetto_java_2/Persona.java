5cc07b9c6acdc966a514586a8bc708bd5c2a64f4
Leonardo Iarossi <iarossi.leonardo@itismeucci.com>
2024-03-30 16:34:30 +0100

    24_03_30 casa

--- progetto_java_2/src/main/java/progetto_java_2/Persona.java
+++ progetto_java_2/src/main/java/progetto_java_2/Persona.java
@@ -18,7 +18,7 @@ public class Persona extends Thread{
         try {
             this.t.getS().acquire(); //"Aquisisco" il semaforo, bloccando l'accesso agli altri Thread, che saranno in attesa"
             int tempo = t.entra(this); //Faccio entrare una persona (thread) in bagno (risorsa) e salvo il tempo occupato
-            System.out.println(this.getName() + " occupa il bagno per: " + tempo + "s");
+            System.out.println(this.getName() + " occupa il bagno per: " + ((int)tempo/1000) + "s");
             Thread.sleep(tempo); //blocco il thread per il tempo occupato
             t.esce(); //La persona esce dal bagno
             this.t.getS().release(); //"Rilascio il semaforo, liberando l'accesso alla risorsa per gli altri Thread"
--- progetto_java_2/src/main/java/progetto_java_2/Toilette.java
+++ progetto_java_2/src/main/java/progetto_java_2/Toilette.java
@@ -7,9 +7,9 @@ public class Toilette{
     String persona;
 
     public int entra(Persona p) throws InterruptedException{
-        persona = p.getName();
-        System.out.println(persona+ " è entrato/a in bagno");
-        return (int) (Math.random()*10) + 1;
+        persona = p.getName(); //salvo il nome della persona (thread) che ha occupato il bagno (risorsa)
+        System.out.println(persona + " è entrato/a in bagno");
+        return (int) (Math.random() * 10000) + 1; //Estraggo un tempo casuale di occupazione (moltipliato così tanto per avere ms che serviranno per sleep())
     }
 
     public void esce(){
@@ -17,7 +17,7 @@ public class Toilette{
     }
 
     public Semaphore getS() {
-        return s;
+        return s; //Restituisce il semaforo
     }
 
     
