62c08c46e1ef93c01231435ad5f6a1da42798acc
Leonardo Iarossi <iarossi.leonardo@itismeucci.com>
2024-04-11 19:28:05 +0200

    23_04_24

--- progetto_java_2/src/main/java/progetto_java_2/Persona.java
+++ progetto_java_2/src/main/java/progetto_java_2/Persona.java
@@ -2,12 +2,16 @@ package progetto_java_2;
 
 public class Persona extends Thread{
     private Toilette t;
-    private String nome;
 
     
 
     public Persona(Toilette t, String nome) {
         super(nome);
+        if((nome.equals("Sandra")) || (nome.equals("Sara"))){
+            this.setPriority(MAX_PRIORITY);
+        }
+        else
+            this.setPriority(MIN_PRIORITY);
         this.t = t;
     }
 
@@ -16,12 +20,10 @@ public class Persona extends Thread{
     @Override
     public void run() {
         try {
-            this.t.getS().acquire(); //"Aquisisco" il semaforo, bloccando l'accesso agli altri Thread, che saranno in attesa"
             int tempo = t.entra(this); //Faccio entrare una persona (thread) in bagno (risorsa) e salvo il tempo occupato
             System.out.println(this.getName() + " occupa il bagno per: " + ((int)tempo/1000) + "s");
             Thread.sleep(tempo); //blocco il thread per il tempo occupato
             t.esce(); //La persona esce dal bagno
-            this.t.getS().release(); //"Rilascio il semaforo, liberando l'accesso alla risorsa per gli altri Thread"
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- progetto_java_2/src/main/java/progetto_java_2/Toilette.java
+++ progetto_java_2/src/main/java/progetto_java_2/Toilette.java
@@ -1,25 +1,32 @@
 package progetto_java_2;
 
-import java.util.concurrent.Semaphore;
+public class Toilette {
+    private boolean disp = true;
+    int contaDonne = 2;
 
-public class Toilette{
-    private Semaphore s = new Semaphore(1);
-    String persona;
-
-    public int entra(Persona p) throws InterruptedException{
-        persona = p.getName(); //salvo il nome della persona (thread) che ha occupato il bagno (risorsa)
+    public synchronized int entra(Persona p) throws InterruptedException {
+        String persona = p.getName(); // salvo il nome della persona (thread) che ha occupato il bagno (risorsa)
+        try {
+            while ((!disp) || (((!p.getName().equals("Sandra")) && (!p.getName().equals("Sara"))) && (contaDonne > 0))) {
+                System.out.println(persona + " sta aspettando, priorità: " + p.getPriority() + "...");
+                wait();
+            }
+        } catch (InterruptedException e) {}
+        disp = false;
         System.out.println(persona + " è entrato/a in bagno");
-        return (int) (Math.random() * 10000) + 1; //Estraggo un tempo casuale di occupazione (moltipliato così tanto per avere ms che serviranno per sleep())
+        if((persona.equals("Sandra")) || (persona.equals("Sara"))){
+            contaDonne--;
+            System.out.println("Donna entrata");
+        }
+        return (int) (Math.random() * 10000) + 1; // Estraggo un tempo casuale di occupazione (moltipliato così tanto
+                                                  // per avere ms che serviranno per sleep())
     }
 
-    public void esce(){
-        System.out.println(persona + " ha lasciato il bagno");
+    public synchronized void esce() {
+        String persona = Thread.currentThread().getName();
+        System.out.println(persona + " ha lasciato il bagno" + "\n---------------------");
+        disp = true;
+        notifyAll();
     }
-
-    public Semaphore getS() {
-        return s; //Restituisce il semaforo
-    }
-
-    
 
 }
