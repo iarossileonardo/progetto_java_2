d45cf3dbc0a1bec2fb7d2320b8b1dc524ec63ab2
Leonardo Iarossi <iarossi.leonardo@itismeucci.com>
2024-04-13 12:57:38 +0200

    ogni tre donne, un'uomo entra

--- progetto_java_2/src/main/java/progetto_java_2/Main.java
+++ progetto_java_2/src/main/java/progetto_java_2/Main.java
@@ -3,13 +3,13 @@ package progetto_java_2;
 public class Main {
     public static void main(String[] args) {
         //I nomi (i Thread) di chi userà il bagno (la risorsa)
-        String [] nomi = {"Marco", "Sara", "Antonella", "Concetta", "Sandra", "Marta", "Giovanna", "Samuela", "Gioele", "Marcoantonio"};
+        String [] nomi = {"Marco", "Sara", "Antonella", "Concetta", "Sandra", "Marta", "Giovanna", "Samuela", "Gioele", "Marcoantonio", "Maicol"};
 
         //Il bagno, ovvero la risorsa che verrà usata
         Toilette t = new Toilette();
 
         //Creao un'aray di oggetti persona (i thread)
-        Persona[] persone = new Persona[10];
+        Persona[] persone = new Persona[11];
 
         //riempio l'array di oggetti persona
         persone[0] = new Persona(t, nomi[0], false);
@@ -22,8 +22,9 @@ public class Main {
         persone[7] = new Persona(t, nomi[7], true);
         persone[8] = new Persona(t, nomi[8], false);
         persone[9] = new Persona(t, nomi[9], false);
+        persone[9] = new Persona(t, nomi[10], false);
 
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 11; i++){
             persone[i].start();
         }
     }
--- progetto_java_2/src/main/java/progetto_java_2/Toilette.java
+++ progetto_java_2/src/main/java/progetto_java_2/Toilette.java
@@ -2,27 +2,31 @@ package progetto_java_2;
 
 public class Toilette {
     private boolean disp = true;
+    int contaUomini;
     int contaDonne = 0;
     int donneEntrate = 0;
 
     public synchronized int entra(Persona p) throws InterruptedException {
         String persona = p.getName(); // salvo il nome della persona (thread) che ha occupato il bagno (risorsa)
         if(p.isDonna()) contaDonne++;
+        else contaUomini ++;
         try {
-            while ((!disp) || (((!p.isDonna()) && (contaDonne > 0) && (donneEntrate < 3)))) {
+            while ((!disp) || (((!p.isDonna()) && (contaDonne > 0) && ((donneEntrate % 3 != 0) || (donneEntrate == 0)))) || ((p.isDonna()) && (donneEntrate % 3 == 0) && (contaUomini > 0) && (donneEntrate != 0))) {
                 System.out.println(persona + " sta aspettando...");
                 wait();
             }
         } catch (InterruptedException e) {}
+        //turnoUomo = false;
         disp = false;
         System.out.println(persona + " è entrato/a in bagno");
         if(p.isDonna()){
             donneEntrate++;
-            if(donneEntrate > 3){
-                donneEntrate = 0;
-            }
             contaDonne--;
             System.out.println("Donna entrata...DONNA");
+        }
+        else{
+            contaUomini--;
+            donneEntrate = 0;
         }
         return (int) (Math.random() * 10000) + 1; // Estraggo un tempo casuale di occupazione (moltipliato così tanto
                                                   // per avere ms che serviranno per sleep())
